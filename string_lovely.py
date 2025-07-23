c4bd762e235abff08a90c99bf57187b0c972c780
Amirthar <amirtha.r14@gmail.com>
2025-07-23 22:14:57 +0530

    Initial DAG commit

---
+++ string_lovely.py
@@ -0,0 +1,24 @@
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'string',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='string_lovely',
+    default_args=default_args,
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['user_generated'],
+) as dag:
+
+    BashOperator(
+        task_id='hello_string',
+        bash_command='echo Hello from string!',
+    )
---
+++ test1_rawy.py
@@ -0,0 +1,24 @@
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'test1',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='test1_rawy',
+    default_args=default_args,
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['user_generated'],
+) as dag:
+
+    BashOperator(
+        task_id='hello_test1',
+        bash_command='echo Hello from test1!',
+    )
